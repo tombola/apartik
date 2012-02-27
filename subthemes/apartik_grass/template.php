08744d5f4159a082fb947a05ba0f92f3e49e3c87
Tom Readings <thomas.readings@falmouth.ac.uk>
2012-02-27 15:37:00 +0000

    apartik_green renamed apartik_grass

---
+++ subthemes/apartik_grass/template.php
@@ -0,0 +1,22 @@
+<?php
+
+
+/**
+ * Override or insert variables into the page template for HTML output.
+ */
+function apartik_grass_process_html(&$variables) {
+  // Hook into color.module.
+  if (module_exists('color')) {
+    _color_html_alter($variables);
+  }
+}
+
+/**
+ * Override or insert variables into the page template.
+ */
+function apartik_grass_process_page(&$variables) {
+  // Hook into color.module.
+  if (module_exists('color')) {
+    _color_page_alter($variables);
+  }
+}
