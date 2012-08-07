bd3d1e67f7bdc642623a7a8255c3f7e797a7796d
Tom Readings <digitisatom@gmail.com>
2012-08-07 11:16:38 +0100

    homebox theme template to add block_titlelink to views blocks (so that can click to go to edit page)

---
+++ homebox-block.tpl.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @file
+ * homebox-block.tpl.php
+ * Default theme implementation each homebox block.
+ */
+if (function_exists('_block_titlelink_get_data')) {
+  $titlelink = _block_titlelink_get_data($block); 
+  $titlelink = url($titlelink['url']);
+}
+?>
+<div id="homebox-block-<?php print $block->key; ?>" class="<?php print $block->homebox_classes ?> clear-block block block-<?php print $block->module ?>">
+  <div class="homebox-portlet-inner">
+    <h3 class="portlet-header">
+      <?php if ($block->closable): ?>
+        <a class="portlet-icon portlet-close"></a>
+      <?php endif; ?>
+      <a class="portlet-icon portlet-maximize"></a>
+      <a class="portlet-icon portlet-minus"></a>
+      <?php if ($page->settings['color'] || isset($block->edit_form)): ?>
+        <a class="portlet-icon portlet-settings"></a>
+      <?php endif; ?>
+      <span class="portlet-title"><a href="<?php print $titlelink; ?>"><?php print $block->subject ?></a></span>
+    </h3>
+    <div class="portlet-config">
+      <?php if ($page->settings['color']): ?>
+        <div class="clear-block"><div class="homebox-colors">
+          <span class="homebox-color-message"><?php print t('Select a color') . ':'; ?></span>
+          <?php for ($i=0; $i < HOMEBOX_NUMBER_OF_COLOURS; $i++): ?>
+            <span class="homebox-color-selector" style="background-color: <?php print $page->settings['colors'][$i] ?>;">&nbsp;</span>
+          <?php endfor ?>
+        </div></div>
+      <?php endif; ?>
+      <?php if (isset($block->edit_form)): print $block->edit_form; endif; ?>
+    </div>
+     <div class="portlet-content content"><?php if (is_string($block->content)){ print $block->content; } else { print drupal_render($block->content); } ?></div>
+  </div>
+</div>
