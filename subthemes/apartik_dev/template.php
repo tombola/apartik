095325b43211c39f7135cf92f558bc0a59498d99
Tom Readings <digitisatom@gmail.com>
2012-08-23 16:16:12 +0100

    date form element changed from fieldset to div

--- subthemes/apartik_dev/template.php
+++ subthemes/apartik_dev/template.php
@@ -23,8 +23,34 @@ function apartik_dev_process_page(&$variables) {
   $user_roles = $user->roles;
   //unset($user_roles[2]); // authenticated? really? such a long word
   $user_roles = implode(' | ', $user_roles);
-  $user_roles = str_replace('research', '', $user_roles);
+  $user_roles = str_replace('research ', '', $user_roles);
   $user_roles = str_replace('authenticated', '*', $user_roles);
   $user_roles = str_replace(' user', '', $user_roles);
   $variables['page']['user_roles'] = '<h3>'.$user_roles.'<h3>';
+}
+
+/**
+ * Returns HTML for a start/end date combination on form.
+ * taken from patch http://drupal.org/node/1467712#comment-6137556
+ */
+function apartik_dev_date_combo($variables) {
+ $element = &$variables['element'];
+
+  // Detect whether element is multiline.
+  $count = preg_match_all('`<(?:div|span)\b[^>]* class="[^"]*\b(?:date-no-float|date-clear)\b`', $element['#children'], $matches, PREG_OFFSET_CAPTURE);
+  $multiline = FALSE;
+  if ($count > 1) {
+    $multiline = TRUE;
+  }
+  elseif ($count) {
+    $before = substr($element['#children'], 0, $matches[0][0][1]);
+    if (preg_match('`<(?:div|span)\b[^>]* class="[^"]*\bdate-float\b`', $before)) {
+      $multiline = TRUE;
+    }
+  }
+
+  // Wrap children with a div and add an extra class if element is multiline.
+  $element['#children'] = '<div class="date-form-element-content'. ($multiline ? ' date-form-element-content-multiline' : '') .'">'. $element['#children'] .'</div>';
+
+  return theme('form_element', $variables);
 }
