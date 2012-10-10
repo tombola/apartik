e3f0edf939090b3850b102e2282c90bd42b93692
Tom Readings <digitisatom@gmail.com>
2012-10-10 10:08:37 +0100

    template function added to dev to hide name-title if Mr, mrs or ms

--- subthemes/apartik_dev/template.php
+++ subthemes/apartik_dev/template.php
@@ -54,3 +54,33 @@ function apartik_dev_date_combo($variables) {
 
   return theme('form_element', $variables);
 }
+
+// Name module includes all titles - hide unless professional
+function apartik_dev_preprocess_field(&$variables) {
+  if($variables['element']['#field_name'] == 'field_title_name') {
+    //if($variables['items']['0']['#markup'] == 'thedefaultvalue') {
+      $variables['items']['0']['#markup'] = _hide_name_titles($variables['items']['0']['#markup']); //preg_replace('/^Mr./','', $variables['items']['0']['#markup']);
+    //}
+  }
+}
+// Name module includes all titles - hide unless professional
+// altered from name.module to vary rendering according to title (eg Mr.)
+function apartik_dev_username_alter(&$name, $account) {
+  // Don't alter anonymous users or objects that do not have any user ID.
+  if (empty($account->uid)) {
+    return;
+  }
+  // Real name was loaded/generated via hook_user_load(), so re-use it.
+  if (!empty($account->realname)) {
+    $name = _hide_name_titles($account->realname);
+  }
+}
+
+function _hide_name_titles($name) {
+   $hidden_titles = array(
+      'Mr.',
+      'Ms.',
+      'Mrs.',
+      );
+    return str_replace($hidden_titles, '', $name);
+}
--- template.php
+++ template.php
@@ -20,3 +20,28 @@ function apartik_process_page(&$variables) {
     _color_page_alter($variables);
   }
 }
+
+
+function apartik_preprocess_field(&$variables) {
+  if($variables['element']['#field_name'] == 'field_title_name') {
+    //if($variables['items']['0']['#markup'] == 'thedefaultvalue') {
+      $variables['items']['0']['#markup'] = preg_replace('/^Mr./','', $variables['items']['0']['#markup']);
+    //}
+  }
+}
+// altered from name.module to vary rendering according to title (eg no Mr.)
+function apartik_username_alter(&$name, $account) {
+  // Don't alter anonymous users or objects that do not have any user ID.
+  if (empty($account->uid)) {
+    return;
+  }
+  // Real name was loaded/generated via hook_user_load(), so re-use it.
+  if (!empty($account->realname)) {
+    $hidden_titles = array(
+      'Mr.',
+      'Ms.',
+      'Mrs.',
+      );
+    $name = str_replace($hidden_titles, '', $account->realname);
+  }
+}
